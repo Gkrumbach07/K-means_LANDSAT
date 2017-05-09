b94b8657d39a8bf8bfefbb119fb143070633cee2
gkrumbach <gkrumbach@gmail.com>
2017-05-09 10:03:43 -0400

    changed anming conventions

--- dataGrabber.py
+++ dataGrabber.py
@@ -5,29 +5,29 @@ from gdalconst import *
 
 
 class LandsatImageData:
-    dataMin = None
-    dataMax = None
-    dataSet = None
+    data_min = None
+    data_max = None
+    dat_set = None
     band = None
 
     def __init__(self, data_file_path):
-        self.dataSet = gdal.Open(data_file_path, GA_ReadOnly)
-        self.band = self.dataSet.GetRasterBand(1)
+        self.data_set = gdal.Open(data_file_path, GA_ReadOnly)
+        self.band = self.data_set.GetRasterBand(1)
         self.dataMin = self.band.GetMinimum()
         self.dataMax = self.band.GetMaximum()
 
-    def UnpackData(self, xoff, yoff, xsize, ysize):
-        scanArea = self.band.ReadRaster(xoff, yoff, xsize, ysize, xsize, ysize, GDT_Float32)
-        floatTuple = struct.unpack('f' * xsize * ysize, scanArea)
-        return floatTuple
+    def upack_data(self, xoff, yoff, xsize, ysize):
+        scan_area = self.band.ReadRaster(xoff, yoff, xsize, ysize, xsize, ysize, GDT_Float32)
+        float_tuple = struct.unpack('f' * xsize * ysize, scan_area)
+        return float_tuple
 
-    def thinData(self, float_tuple, iteration):
-        newData = []
+    def thin_data(self, float_tuple, iteration):
+        new_data = []
         for i in range(float_tuple.__len__()):
             if i % iteration == 0:
-                newData.append(float_tuple[i])
-        return newData
+                new_data.append(float_tuple[i])
+        return new_data
 
 
-blueBand = LandsatImageData("landsat_tif\B2.TIF")
-print blueBand.thinData(blueBand.unpackData(0, 3000, blueBand.band.XSize, 1), 1)
+blue_band = LandsatImageData("landsat_tif\B2.TIF")
+print blue_band.thin_data(blue_band.upack_data(0, 3000, blue_band.band.XSize, 1), 1)
