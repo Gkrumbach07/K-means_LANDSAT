6b93b3bf1dc79db2ab3df9228958d8e33f057bbc
gkrumbach <gkrumbach@gmail.com>
2017-05-11 09:56:22 -0400

    finished basic data graber functions

--- dataGrabber.py
+++ dataGrabber.py
@@ -7,34 +7,36 @@ from gdalconst import *
 class LandsatImageData:
     data_min = None
     data_max = None
-    dat_set = None
     band = None
+    iteration = 100
+    compiled_data = []
 
-    def __init__(self, data_file_path):
-        self.data_set = gdal.Open(data_file_path, GA_ReadOnly)
-        self.band = self.data_set.GetRasterBand(1)
-        self.dataMin = self.band.GetMinimum()
+    def __init__(self, data_file_path, iterate):
+        data_set = gdal.Open(data_file_path, GA_ReadOnly)
+        self.band = data_set.GetRasterBand(1)
+        self.dataMin = self.band.GetMinimum() #16-bit =  0-65535
         self.dataMax = self.band.GetMaximum()
+        self.iteration = iterate
 
     def upack_data(self, xoff, yoff, xsize, ysize):
         scan_area = self.band.ReadRaster(xoff, yoff, xsize, ysize, xsize, ysize, GDT_Float32)
         float_tuple = struct.unpack('f' * xsize * ysize, scan_area)
         return float_tuple
 
-    def thin_data(self, float_tuple, iteration):
-        new_data = []
-        for i in range(float_tuple.__len__()):
-            if i % iteration == 0:
+    def thin_data(self, float_tuple):
+        for i in range(float_tuple.__len__()): # ++n
+            if i % self.iteration == 0:
                 if float_tuple[i] != 0: #OPTIMIZE--- Start from middle and go until n number of no data pixles show up
-                    new_data.append(float_tuple[i])
-        return new_data
+                    compiled_data.append(normalize(float_tuple[i], data_min, data_max)
     
     def create_dataset(self):
-      compiled_data = [] #too many arrays -- reduce 
       for i in range(self.band.YSize):
-        compiled_data.append(self.thin_data(self.upack_data(0, 0, self.band.XSize, i), 10))
+        self.thin_data(self.upack_data(0, i, self.band.XSize, 1), self.iteration)
+        
+    def normalize(self, value, min, max):
+      normalized = (value - min) / (max - min)
+      return normalized
 
 
-blue_band = LandsatImageData("landsat_tif\B2.TIF")
-print blue_band.thin_data(blue_band.upack_data(0, 3000, blue_band.band.XSize, 1), 1)
-#16-bit =  0-65535
+blue_band = LandsatImageData("landsat_tif\B2.TIF", 100)
+blue_band.create_dataset()
