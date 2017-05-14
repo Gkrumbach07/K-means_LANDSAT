0ed1e4dc46d736ad755074d93b56a3ff34411aa6
Gage Krumbach <gkrumbach@gmail.com>
2017-05-14 17:21:29 -0500

    fixed data grabber
    opended band was getting corrupted. Made it a class variable.

--- dataGrabber.py
+++ dataGrabber.py
@@ -1,6 +1,5 @@
 import gdal
 import struct
-import numpy as np
 from gdalconst import *
 
 
@@ -10,33 +9,39 @@ class LandsatImageData:
     band = None
     iteration = 100
     compiled_data = []
+    data_set = None
 
     def __init__(self, data_file_path, iterate):
-        data_set = gdal.Open(data_file_path, GA_ReadOnly)
-        self.band = data_set.GetRasterBand(1)
-        self.dataMin = self.band.GetMinimum() #16-bit =  0-65535
-        self.dataMax = self.band.GetMaximum()
+        self.data_set = gdal.Open(data_file_path, GA_ReadOnly)
+        self.band = self.data_set.GetRasterBand(1)
+        self.band.DeleteNoDataValue()
+        (self.data_min, self.data_max) = self.band.ComputeRasterMinMax(1)
         self.iteration = iterate
 
-    def upack_data(self, xoff, yoff, xsize, ysize):
+    def unpack_data(self, xoff, yoff, xsize, ysize):
         scan_area = self.band.ReadRaster(xoff, yoff, xsize, ysize, xsize, ysize, GDT_Float32)
         float_tuple = struct.unpack('f' * xsize * ysize, scan_area)
         return float_tuple
 
-    def thin_data(self, float_tuple):
-        for i in range(float_tuple.__len__()): # ++n
-            if i % self.iteration == 0:
-                if float_tuple[i] != 0: #OPTIMIZE--- Start from middle and go until n number of no data pixles show up
-                    compiled_data.append(normalize(float_tuple[i], data_min, data_max)
-    
     def create_dataset(self):
-      for i in range(self.band.YSize):
-        self.thin_data(self.upack_data(0, i, self.band.XSize, 1), self.iteration)
-        
+        for i in range(self.band.YSize):
+            if i % self.iteration == 0:
+                self.thin_data(self.unpack_data(0, i, self.band.XSize, 1))
+
     def normalize(self, value, min, max):
-      normalized = (value - min) / (max - min)
-      return normalized
+        normalized = (value - min) / (max - min)
+        return normalized
+
+    def thin_data(self, float_tuple):
+        for i in range(float_tuple.__len__()):  # ++n
+            if i % self.iteration == 0:
+                if float_tuple[i] != 0:  # OPTIMIZE--- Start from middle and go until n number of no data pixles show up
+                    self.compiled_data.append(self.normalize(float_tuple[i], self.data_min, self.data_max))
 
 
 blue_band = LandsatImageData("landsat_tif\B2.TIF", 100)
 blue_band.create_dataset()
+print sorted(blue_band.compiled_data)
+print blue_band.compiled_data.__len__()
+print blue_band.data_max
+print blue_band.data_min
